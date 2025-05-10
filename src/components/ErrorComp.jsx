431ad8e35cd2d2791ef195e7c6f53363417b6f7d
Muba-rak <oladapomubarak@gmail.com>
2025-05-10 12:28:55 +0100

    made errror

---
+++ src/components/ErrorComp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorComp = () => {
+  return <div>ErrorComp</div>;
+};
+
+export default ErrorComp;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "../components/Nav";
+import ErrorComp from "../components/Errorcomp";
 
 const Home = () => {
   return (
     <div>
       <Nav />
+      <ErrorComp />
       Hero WhyTech
     </div>
   );
