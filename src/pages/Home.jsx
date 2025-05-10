4e719bb1ac361ba3dab07c413d195906d35d2c97
Muba-rak <oladapomubarak@gmail.com>
2025-05-10 12:30:39 +0100

    fixed bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "../components/Nav";
-import ErrorComp from "../components/Errorcomp";
+import ErrorComp from "../components/ErrorComp";
 
 const Home = () => {
   return (
